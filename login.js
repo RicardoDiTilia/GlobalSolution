35fcaadc3d8587bd76fdacc74aa87f489050f466
Gabriel Lilla <gabrielterralilla@gmail.com>
2024-06-05 23:22:53 -0300

    att

--- login.js
+++ login.js
@@ -1,14 +1,18 @@
 // login.js
 
-function login() {
+document.getElementById('login-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
-  // Simulação de lógica de login
-  if (username === 'admin' && password === 'admin') {
+  const storedUsername = localStorage.getItem('username');
+  const storedPassword = localStorage.getItem('password');
+
+  if (username === storedUsername && password === storedPassword) {
       alert('Login bem-sucedido!');
-      // Redirecionar para outra página ou realizar outras ações de login
+      window.location.href = 'main.html';
   } else {
-      alert('Usuário ou senha incorretos.');
+      alert('Usuário ou senha incorretos!');
   }
-}
+});
